5ff8903971a4f63fd2d3f33cdb5da09bc867fd09
Megan Uyen Nguyen <megannguyen@berkeley.edu>
2026-03-13 13:03:29 -0700

    changed x to z

--- the_project_file.py
+++ the_project_file.py
@@ -6,7 +6,4 @@ x = 34
 z = x + y
 
 print("The answer is:", z)
-# fixed bug: replaced x with z
-# i believe I saved to the main branch so there is nothing to commit
-
-print("branch_demo change")
+# fixed bug - replaced x with z
