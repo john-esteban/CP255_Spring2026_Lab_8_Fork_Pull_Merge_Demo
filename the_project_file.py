ff4b87d4589d25d632cc6b3523e5250c6eab1af3
Megan Uyen Nguyen <megannguyen@berkeley.edu>
2026-03-13 12:54:43 -0700

    Update the_project_file.py

--- the_project_file.py
+++ the_project_file.py
@@ -5,4 +5,8 @@ x = 34
 
 z = x + y
 
-print("The answer is:", x)
+print("The answer is:", z)
+# fixed bug: replaced x with z
+# i believe I saved to the main branch so there is nothing to commit
+
+print("branch_demo change")
